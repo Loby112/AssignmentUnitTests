60e60269525b39728d65797731592e9e3d91e202
Tobias <tobi195h@edu.easj.dk>
2022-03-07 22:29:20 +0100

    Add project files.

---
+++ AssignmentUnitTests/Car.cs
@@ -0,0 +1,49 @@
+﻿namespace AssignmentUnitTests
+{
+    public class Car {
+        private string _licensePlate;
+        private string _model;
+        private double _price;
+        private static int _nextID = 1;
+
+        public Car(string model, double price, string licensePlate){
+            ID = _nextID;
+            _nextID++;
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+
+        public int  ID { get; }
+
+        public string Model{
+            get => _model;
+            set{
+                if (value.Length < 4){
+                    throw new ArgumentException("Model name must be at least 4 characters");
+                }
+                _model = value;
+            }
+        }
+
+        public double Price{
+            get => _price;
+            set{
+                if (value <= 0){
+                    throw new ArgumentOutOfRangeException("Price must be higher than 0");
+                }
+                _price = value;
+            }
+        }
+
+        public string LicensePlate{ 
+            get => _licensePlate;
+            set{
+                if (value.Length < 2 || value.Length > 7){
+                    throw new ArgumentException("License plate must be 2 or more characters and max 7");
+                }
+                _licensePlate = value;
+            }
+        }
+    }
+}   
---
+++ AssignmentUnitTestsTests/CarTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssignmentUnitTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentUnitTests.Tests {
+    [TestClass()]
+    public class CarTests{
+        [TestMethod()]
+        public void CarTest(){
+            Car car = new Car("Mercedes", 700000, "XE3215S");
+            Car car2 = new Car("Four", 1, "LE");
+            Assert.AreEqual("Mercedes", car.Model);
+            Assert.AreEqual(1, car.ID);
+            Assert.AreEqual(700000, car.Price);
+            Assert.AreEqual("XE3215S", car.LicensePlate);
+            Assert.AreEqual("Four", car2.Model);
+            Assert.AreEqual("LE", car2.LicensePlate);
+            Assert.ThrowsException<ArgumentException>(() => car.LicensePlate = "S");
+            Assert.ThrowsException<ArgumentException>(() => car.LicensePlate = "TooLongX");
+            Assert.ThrowsException<ArgumentException>(() => car.Model = "dk2");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => car.Price = -1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => car.Price = 0);
+            
+        }
+        [TestMethod]
+        public void ConstructorTest(){
+            try{
+                Car car = new Car("Ipu", 3000, "SKSS2");
+            }
+            catch (ArgumentException e){
+                Assert.IsNotNull(e.Message);
+            }
+
+            try{
+                Car car = new Car("SuperCarModel", 0, "SCM3000");
+            }
+            catch(ArgumentOutOfRangeException rangeException){
+                Assert.IsNotNull(rangeException.Message);
+            }
+
+            try{
+                Car car = new Car("DifferentModel", 2, "X");
+            }
+            catch (ArgumentException plateTooShort){
+                Assert.IsNotNull(plateTooShort.Message);
+            }
+
+            try{
+                Car car = new Car("AnotherCarModel", 50000, "ToolongX");
+            }
+            catch (ArgumentException plateTooLong){
+                Assert.IsNotNull(plateTooLong.Message);
+            }
+
+        }
+    }
+}       
